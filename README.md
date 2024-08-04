dbd57af861f12b03f0b3b8f83ee9a44c7b12856b
Sam Atman <atmanistan@gmail.com>
2024-08-04 11:38:20 -0400

    Begin obelizmo.zig
    A library for marking up strings.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Obelizmo: A String Marking Library
+
+This library provides a flexible system for marking up strings, a practice referred to in ancient days as [Obelism](https://en.wikipedia.org/wiki/Obelism).
+
+Further documentation will be forthcoming.
---
+++ build.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Export as module to be available for @import("mvzr") on user site
+    _ = b.addModule("obelizmo", .{
+        .root_source_file = b.path("src/obelizmo.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_filters = b.option([]const []const u8, "test-filter", "Skip tests that do not match any filter") orelse &[0][]const u8{};
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/obelizmo.zig"),
+        .target = target,
+        .optimize = optimize,
+        .filters = test_filters,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
