cb2cd9f31f6c9114f5877699fc66733e902d2482
Sam Atman <atmanistan@gmail.com>
2024-08-05 07:32:47 -0400

    Add writeAsStream
    Untested as of yet.  This is the more difficult algorithm of the two in
    that we have to juggle the out queue to re-start styles when closing an
    inner style, so that ANSI escapes will work correctly.
    
    I should circle back and write a buffered iterator for this, as well as
    the upcoming writeAsTree, so that write-and-flush workloads are doable.

--- src/obelizmo.zig
+++ src/obelizmo.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 const PriorityQueue = std.PriorityQueue;
 const Order = std.math.Order;
@@ -13,19 +14,33 @@ pub fn StringMarker(Kind: type) type {
         kind: Kind,
         offset: u32,
         len: u32,
-    };
 
-    const MarkedString = struct {
-        string: []const u8,
-        marks: []const Mark,
+        fn final(mark: @This()) u32 {
+            return mark.offset + mark.len;
+        }
+
+        fn value(mark: @This()) usize {
+            return @intFromEnum(mark.kind);
+        }
     };
 
     return struct {
         string: []const u8,
         queue: MarkQueue,
 
-        const MarkQueue = PriorityQueue(Mark, void, compare);
         const SMark = @This();
+
+        /// Queue for applying Marks.
+        const MarkQueue = PriorityQueue(Mark, void, compare);
+
+        /// Queue for writing Marks.
+        const SweepQueue = PriorityQueue(Mark, void, compareEnds);
+
+        /// The EnumArray type expected by StringMarker printing functions.
+        /// Initialized with a value of `[2][]const u8`, representing a pair of
+        /// bookends for printing the marked string.  For information in setting
+        /// up and using this type, see `std.enums.EnumArray`.
+        pub const MarkupArray = std.enums.EnumArray(Kind, [2][]const u8);
 
         /// Initialize a StringMarker.  The string is not considered to be
         /// owned by the StringMarker, as such, the caller is responsible
@@ -47,7 +62,7 @@ pub fn StringMarker(Kind: type) type {
             string: []const u8,
             cap: usize,
         ) error{OutOfMemory}!SMark {
-            const m_queue = MarkQueue.init(allocator, void);
+            const m_queue = MarkQueue.init(allocator, {});
             try m_queue.ensureTotalCapacity(cap);
             return SMark{
                 .string = string,
@@ -124,6 +139,9 @@ pub fn StringMarker(Kind: type) type {
             } else return false;
         }
 
+        const LEFT: usize = 0;
+        const RIGHT: usize = 1;
+
         /// Find last occurence of `needle` in string, and mark with
         /// `mark`.  Returns `true` if the needle was found and marked,
         /// `false` otherwise.
@@ -139,26 +157,82 @@ pub fn StringMarker(Kind: type) type {
             } else return false;
         }
 
-        /// Turn the StringMarker into a MarkedString.  Copies the
-        /// Marks, StringMarker must still be freed.
-        pub fn toMarkedString(marker: *SMark) !MarkedString {
-            const marks = try marker.queue.allocator.alloc(Mark, marker.queue.items.len);
-            @memcpy(marks, marker.queue.items);
-            return MarkedString{
-                .string = marker.string,
-                .marks = marks,
-            };
-        }
+        /// Write out the marked string as a stream: this is designed to
+        /// work with protocols like ANSI terminal sequences, where the
+        /// style signaling is in-band, and has to be repeated in order
+        /// for nested colors to function correctly.  If emitting a tree-
+        /// shaped markup syntax, such as XML or HTML, use `writeAsTree`.
+        pub fn writeAsStream(
+            marker: *const SMark,
+            writer: anytype,
+            markups: *const MarkupArray,
+        ) !void {
+            // We use a second queue with a different comparison function, such
+            // that the front of the queue is always the next-outermost Mark.
+            const allocator = marker.queue.allocator;
+            const string = marker.string;
+            const in_q = marker.queue;
+            const out_q = SweepQueue.init(allocator, {});
+            var this_mark = in_q.removeOrNull();
+            var cursor: usize = 0;
+            marking: while (this_mark) |mark| {
+                const maybe_next = out_q.peek();
+                var from_this_mark = true; // determines where we get our offset
+                const next_idx = idx: {
+                    if (maybe_next) |next_mark| {
+                        const next_mark_end = next_mark.final();
+                        if (next_mark_end < mark.offset) {
+                            from_this_mark = false;
+                            break :idx next_mark_end;
+                        } else break :idx mark.offset;
+                    } else {
+                        break :idx mark.offset;
+                    }
+                };
+                if (cursor < next_idx) {
+                    try writer.writeAll(string[cursor..next_idx]);
+                }
+                cursor = next_idx;
+                if (from_this_mark) {
+                    // Write our bookend.
+                    const left = markups.get(mark.kind)[LEFT];
+                    try writer.writeAll(left);
+                    // Enplace on the out queue.
+                    try out_q.add(mark);
+                    // Replace mark.
+                    this_mark = in_q.removeOrNull();
+                    continue :marking;
+                } else {
+                    // This mark isn't up yet, write the end off the queue.
+                    const end_mark = out_q.remove();
+                    const right = markups.get(end_mark.kind)[RIGHT];
+                    try writer.writeAll(right);
+                    // Now stream the left mark from the next on-queue, if any.
+                    const maybe_left_mark = out_q.peek();
+                    if (maybe_left_mark) |left_mark| {
+                        const left = markups.get(left_mark.kind)[LEFT];
+                        try writer.writeAll(left);
+                    }
+                    continue :marking;
+                }
+            } // end :marking
+            // There may still be marks on the out queue to drain
+            while (out_q.removeOrNull()) |out_mark| {
+                const slice_end = out_mark.final();
+                try writer.writeAll(string[cursor..slice_end]);
+                cursor = slice_end;
+                const right = markups.get(out_mark.kind)[RIGHT];
+                try writer.writeAll(right);
+                const maybe_left_mark = out_q.peek();
+                if (maybe_left_mark) |left_mark| {
+                    const left = markups.get(left_mark.kind)[LEFT];
+                    try writer.writeAll(left);
+                }
+            }
+            // Write the rest of the string, if any
+            try writer.writeAll(string[cursor..]);
 
-        /// Transfers ownership of the StringMarker's []Mark slice to
-        /// a MarkedString.  Leaves the StringMarker in an invalid
-        /// state, do not call `deinit`!
-        pub fn toOwnedMarkedString(marker: *SMark) MarkedString {
-            marker.queue.shrinkAndFree(marker.queue.items.len);
-            return MarkedString{
-                .string = marker.string,
-                .marks = marker.queue.items,
-            };
+            return;
         }
 
         /// Our sort will yield an in-order top down tree:
@@ -183,6 +257,25 @@ pub fn StringMarker(Kind: type) type {
                 return Order.eq;
             }
         }
+
+        /// This compare function is used for the out queue,
+        /// so it only cares about offset + len.  The tie-breaker
+        /// by enum is in the opposite order, so that two different
+        /// enums of the same offset and len will be applied in the
+        /// correct order, such that one nests within the other.
+        fn compareEnds(_: void, left: Mark, right: Mark) Order {
+            const l_final = left.final();
+            const r_final = right.final();
+            if (l_final < r_final) {
+                return Order.lt;
+            } else if (l_final > r_final) {
+                return Order.gt;
+            } else if (@intFromEnum(left.kind) < @intFromEnum(right.kind)) {
+                return Order.gt;
+            } else if (@intFromEnum(left.kind) > @intFromEnum(right.kind)) {
+                return Order.lt;
+            } else return Order.eq;
+        }
     };
 }
 
@@ -201,52 +294,35 @@ test "StringMarker" {
         dah,
     };
     const SM = StringMarker(e_num);
-    // We don't free this because we borrow memory from it later
     var markup = SM.init(allocator, "blue green red");
+    defer markup.deinit();
     try markup.markSlice(.la, 0, 4);
     try markup.markSlice(.dee, 0, 11);
     try markup.markSlice(.dah, 11, 14);
-    const marked_string = try markup.toMarkedString();
-    defer allocator.free(marked_string.marks);
     try oh.snap(
         @src(),
-        \\obelizmo.StringMarker.MarkedString
-        \\  .string: []const u8
-        \\    "blue green red"
-        \\  .marks: []const obelizmo.StringMarker.Mark
-        \\    [0]: obelizmo.StringMarker.Mark
-        \\      .kind: obelizmo.test.StringMarker.e_num
-        \\        .dee
-        \\      .offset: u32 = 0
-        \\      .len: u32 = 11
-        \\    [1]: obelizmo.StringMarker.Mark
-        \\      .kind: obelizmo.test.StringMarker.e_num
-        \\        .la
-        \\      .offset: u32 = 0
-        \\      .len: u32 = 4
-        \\    [2]: obelizmo.StringMarker.Mark
-        \\      .kind: obelizmo.test.StringMarker.e_num
-        \\        .dah
-        \\      .offset: u32 = 11
-        \\      .len: u32 = 3
+        \\[]obelizmo.StringMarker.Mark
+        \\  [0]: obelizmo.StringMarker.Mark
+        \\    .kind: obelizmo.test.StringMarker.e_num
+        \\      .dee
+        \\    .offset: u32 = 0
+        \\    .len: u32 = 11
+        \\  [1]: obelizmo.StringMarker.Mark
+        \\    .kind: obelizmo.test.StringMarker.e_num
+        \\      .la
+        \\    .offset: u32 = 0
+        \\    .len: u32 = 4
+        \\  [2]: obelizmo.StringMarker.Mark
+        \\    .kind: obelizmo.test.StringMarker.e_num
+        \\      .dah
+        \\    .offset: u32 = 11
+        \\    .len: u32 = 3
         ,
-    ).expectEqual(marked_string);
-    const owned_marked = markup.toOwnedMarkedString();
-    defer allocator.free(owned_marked.marks);
-    try expectEqualSlices(
-        @TypeOf(marked_string.marks[0]),
-        marked_string.marks,
-        owned_marked.marks,
-    );
-}
-
-test "ohsnap is installed" {
-    const four_tw0: u64 = 42;
-    const oh = OhSnap{};
+    ).expectEqual(markup.queue.items);
     try oh.snap(
         @src(),
-        \\u64
-        \\  42
+        \\type
+        \\  enums.EnumArray(obelizmo.test.StringMarker.e_num,[2][]const u8)
         ,
-    ).expectEqual(four_tw0);
+    ).expectEqual(SM.MarkupArray);
 }
