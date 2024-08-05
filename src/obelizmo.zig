a77d2aba73d11e2875a3c6a704fcd5f334513971
Sam Atman <atmanistan@gmail.com>
2024-08-04 20:23:56 -0400

    Add another marker, transfer functions
    Probably want to go back through and make a flag for when the memory is
    valid, to catch use-after-borrow.

--- src/obelizmo.zig
+++ src/obelizmo.zig
@@ -19,7 +19,6 @@ pub fn StringMarker(Kind: type) type {
         string: []const u8,
         marks: []const Mark,
     };
-    _ = MarkedString;
 
     return struct {
         string: []const u8,
@@ -35,7 +34,7 @@ pub fn StringMarker(Kind: type) type {
         pub fn init(allocator: Allocator, string: []const u8) SMark {
             return SMark{
                 .string = string,
-                .queue = MarkQueue.init(allocator, void),
+                .queue = MarkQueue.init(allocator, {}),
             };
         }
 
@@ -71,10 +70,11 @@ pub fn StringMarker(Kind: type) type {
             start: usize,
             end: usize,
         ) !void {
+            if (start > end) return error.InvalidSliceBoundary;
             const the_mark = Mark{
                 .kind = mark,
-                .offset = start,
-                .len = end - start,
+                .offset = @intCast(start),
+                .len = @intCast(end - start),
             };
             try marker.queue.add(the_mark);
         }
@@ -88,8 +88,8 @@ pub fn StringMarker(Kind: type) type {
         ) !void {
             const the_mark = Mark{
                 .kind = mark,
-                .offset = offset,
-                .len = len,
+                .offset = @intCast(offset),
+                .len = @intCast(len),
             };
             try marker.queue.add(the_mark);
         }
@@ -124,6 +124,43 @@ pub fn StringMarker(Kind: type) type {
             } else return false;
         }
 
+        /// Find last occurence of `needle` in string, and mark with
+        /// `mark`.  Returns `true` if the needle was found and marked,
+        /// `false` otherwise.
+        pub fn findAndMarkLast(
+            marker: *SMark,
+            mark: Kind,
+            needle: []const u8,
+        ) !bool {
+            const idx = std.mem.lastIndexOf(u8, marker.string, needle);
+            if (idx) |i| {
+                try marker.markFrom(mark, i, needle.len);
+                return true;
+            } else return false;
+        }
+
+        /// Turn the StringMarker into a MarkedString.  Copies the
+        /// Marks, StringMarker must still be freed.
+        pub fn toMarkedString(marker: *SMark) !MarkedString {
+            const marks = try marker.queue.allocator.alloc(Mark, marker.queue.items.len);
+            @memcpy(marks, marker.queue.items);
+            return MarkedString{
+                .string = marker.string,
+                .marks = marks,
+            };
+        }
+
+        /// Transfers ownership of the StringMarker's []Mark slice to
+        /// a MarkedString.  Leaves the StringMarker in an invalid
+        /// state, do not call `deinit`!
+        pub fn toOwnedMarkedString(marker: *SMark) MarkedString {
+            marker.queue.shrinkAndFree(marker.queue.items.len);
+            return MarkedString{
+                .string = marker.string,
+                .marks = marker.queue.items,
+            };
+        }
+
         /// Our sort will yield an in-order top down tree:
         /// All marks which start before another mark come before
         /// all later marks, with all longer marks before shorter
@@ -152,16 +189,55 @@ pub fn StringMarker(Kind: type) type {
 //| TESTS
 
 const testing = std.testing;
+const expectEqualSlices = testing.expectEqualSlices;
 const OhSnap = @import("ohsnap");
 
 test "StringMarker" {
+    const allocator = std.testing.allocator;
+    const oh = OhSnap{};
     const e_num = enum {
         la,
         dee,
         dah,
     };
     const SM = StringMarker(e_num);
-    _ = SM;
+    // We don't free this because we borrow memory from it later
+    var markup = SM.init(allocator, "blue green red");
+    try markup.markSlice(.la, 0, 4);
+    try markup.markSlice(.dee, 0, 11);
+    try markup.markSlice(.dah, 11, 14);
+    const marked_string = try markup.toMarkedString();
+    defer allocator.free(marked_string.marks);
+    try oh.snap(
+        @src(),
+        \\obelizmo.StringMarker.MarkedString
+        \\  .string: []const u8
+        \\    "blue green red"
+        \\  .marks: []const obelizmo.StringMarker.Mark
+        \\    [0]: obelizmo.StringMarker.Mark
+        \\      .kind: obelizmo.test.StringMarker.e_num
+        \\        .dee
+        \\      .offset: u32 = 0
+        \\      .len: u32 = 11
+        \\    [1]: obelizmo.StringMarker.Mark
+        \\      .kind: obelizmo.test.StringMarker.e_num
+        \\        .la
+        \\      .offset: u32 = 0
+        \\      .len: u32 = 4
+        \\    [2]: obelizmo.StringMarker.Mark
+        \\      .kind: obelizmo.test.StringMarker.e_num
+        \\        .dah
+        \\      .offset: u32 = 11
+        \\      .len: u32 = 3
+        ,
+    ).expectEqual(marked_string);
+    const owned_marked = markup.toOwnedMarkedString();
+    defer allocator.free(owned_marked.marks);
+    try expectEqualSlices(
+        @TypeOf(marked_string.marks[0]),
+        marked_string.marks,
+        owned_marked.marks,
+    );
 }
 
 test "ohsnap is installed" {
